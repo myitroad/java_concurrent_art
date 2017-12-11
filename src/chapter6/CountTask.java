9387dc40a6408bc86ae3487f2e2bb82df058e235
liutingna <liutingna86@163.com>
2017-12-11 08:51:51 +0800

    Update: 更新ForkJoin示例的注释

--- src/chapter6/CountTask.java
+++ src/chapter6/CountTask.java
@@ -6,10 +6,11 @@ import java.util.concurrent.RecursiveTask;
 
 /**
  * Created by xdcao on 2017/5/21.
+ * 计算联系整数，如1+2+3+4...
  */
 public class CountTask extends RecursiveTask<Integer> {
 
-    private static final int THRESHOLD=2;
+    private static final int THRESHOLD = 2;
 
     private int start;
     private int end;
@@ -22,34 +23,41 @@ public class CountTask extends RecursiveTask<Integer> {
     @Override
     protected Integer compute() {
 
-        int sum=0;
-        boolean canCompute=(end-start)<=THRESHOLD;
+        int sum = 0;
+        //判断是否要进行计算
+        boolean canCompute = (end - start) <= THRESHOLD;
+        /**
+         * 如果任务足够小（这里要小到只有两个数参与计算）
+         */
         if (canCompute) {
-            for (int i=start;i<=end;i++){
-                sum+=i;
+            for (int i = start; i <= end; i++) {
+                sum += i;
             }
-        }else {
-            int middle=(start+end)/2;
-            CountTask leftTask=new CountTask(start,middle);
-            CountTask rightTask=new CountTask(middle+1,end);
+        } else {
+            /**
+             * 若任务可以再分，那么将任务一分为二，即fork阶段
+             */
+            int middle = (start + end) / 2;
+            CountTask leftTask = new CountTask(start, middle);
+            CountTask rightTask = new CountTask(middle + 1, end);
             leftTask.fork();
             rightTask.fork();
-            int leftResult=leftTask.join();
-            int rightResult=rightTask.join();
-            sum=leftResult+rightResult;
+            int leftResult = leftTask.join();
+            int rightResult = rightTask.join();
+            //结果合并，即join阶段
+            sum = leftResult + rightResult;
         }
         return sum;
-
     }
 
-    public static void main(String[] args){
-        ForkJoinPool forkJoinPool=new ForkJoinPool();
-        CountTask countTask=new CountTask(1,4);
-        Future<Integer> result=forkJoinPool.submit(countTask);
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        CountTask countTask = new CountTask(1, 4);
+        Future<Integer> result = forkJoinPool.submit(countTask);
         try {
             System.out.println(result.get());
-        }catch (Exception e){
-
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 
